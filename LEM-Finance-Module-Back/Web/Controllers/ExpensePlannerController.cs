60f69496b72a8bf5404fe772c7059b7711bd48df
Orkson <48963859+Orkson@users.noreply.github.com>
2025-03-22 18:31:52 +0100

    Removed unnecessary code

--- LEM-Finance-Module-Back/Web/Controllers/ExpensePlannerController.cs
+++ LEM-Finance-Module-Back/Web/Controllers/ExpensePlannerController.cs
@@ -1,10 +1,8 @@
 ﻿using Application.ExpensesPlanner.Commands;
 using Application.ExpensesPlanner.Queries;
 using Application.Models;
-using Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Web.Controllers
 {
@@ -28,14 +26,6 @@ namespace Web.Controllers
 
             return Ok(result);
         }
-
-        //[HttpGet("{year}")]
-        //public async Task<ActionResult<ExpensePlanner>> GetExpensePlanner(int year)
-        //{
-        //    var result = await _mediator
-        //        .Send(new GetExpensePlannerByYearQuery { Year = year });
-        //    return Ok(result);
-        //}
 
         [HttpPost]
         public async Task<ActionResult<ExpensePlannerDto>> CreateExpensePlanner([FromBody] CreateServiceCommand command)
@@ -61,56 +51,5 @@ namespace Web.Controllers
             
             return NoContent();
         }
-
-
-
-        //[HttpPost]
-        //public async Task<IActionResult> CreateExpensePlanner([FromBody] CreateExpensePlannerCommand command)
-        //{
-        //    var result = await _mediator.Send(command);
-        //
-        //    return Ok(result);
-        //}
-
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> UpdateExpensePlanner(int id, ExpensePlanner expensePlanner)
-        //{
-        //    if (id != expensePlanner.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-        //
-        //    _context.Entry(expensePlanner).State = EntityState.Modified;
-        //
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!_context.ExpensePlanner.Any(e => e.Id == id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        throw;
-        //    }
-        //
-        //    return NoContent();
-        //}
-
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteExpensePlanner(int id)
-        //{
-        //    var expensePlanner = await _context.ExpensePlanner.FindAsync(id);
-        //    if (expensePlanner == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //
-        //    _context.ExpensePlanner.Remove(expensePlanner);
-        //    await _context.SaveChangesAsync();
-        //
-        //    return NoContent();
-        //}
     }
 }
